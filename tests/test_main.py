53a6a62f4b3baf4a64c09e84ead3a2193a7d772d
f-enye <fpena34@gmail.com>
2024-03-04 06:52:28 -0600

    Add from and too attributes to Body.

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,5 +2,5 @@ from unit_testing_disciplines.main import Body, send_text
 
 
 def test_main():
-    response = send_text(Body(text="hello"))
+    response = send_text(Body(from_="5555555555", to="5555555551", text="hello"))
     assert response["body"].text == "hello"
