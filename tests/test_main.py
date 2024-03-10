370768fb94a69d80ca0fcf5d23b83cd9b3f6aac6
f-enye <fpena34@gmail.com>
2024-03-10 11:59:07 -0500

    Update test to expected values were passed to external_api function.

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,15 +1,16 @@
-from unittest.mock import patch
+from unittest.mock import patch, ANY
 from pytest import fixture
 from unit_testing_disciplines.external.text.send import SendResult
 from unit_testing_disciplines.main import Body, send_text
 
 @fixture
 def send_mock():
-    with patch("unit_testing_disciplines.main.send", return_value=SendResult(id="1", status="sent")):
-        yield
+    with patch("unit_testing_disciplines.main.send", return_value=SendResult(id="1", status="sent")) as mock:
+        yield mock
 
 
 
-def test_main(send_mock):
-    response = send_text(Body(from_="5555555555", to="5555555551", text="hello"))
+def test_main_given_text_is_sent_successfully(send_mock):
+    response = send_text(Body(to="5555555552", from_="5555555551", text="hello"))
+    send_mock.assert_called_once_with(ANY, "sms", "5555555552", "5555555551", "hello")
     assert response.status == "sent"
