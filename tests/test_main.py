682960dbc987f78501dc9e9e68b8f3f678ecc32b
f-enye <fpena34@gmail.com>
2024-03-10 11:36:51 -0500

    Add external request api function and update tests.

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,14 @@
-from unit_testing_disciplines.main import Body, send_text
+from unittest.mock import patch
+from pytest import fixture
+from unit_testing_disciplines.main import Body, send_text, SendResult
+
+@fixture
+def send_mock():
+    with patch("unit_testing_disciplines.main.send", return_value=SendResult(id="1", status="sent")):
+        yield
 
 
-def test_main():
+
+def test_main(send_mock):
     response = send_text(Body(from_="5555555555", to="5555555551", text="hello"))
-    assert response["body"].text == "hello"
+    assert response.status == "sent"
--- unit_testing_disciplines/main.py
+++ unit_testing_disciplines/main.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from typing import Any, Callable
-
+import requests
+from dataclasses import dataclass
 
 def post(path: str) -> Callable[..., Any]:
     def _wrapper(f: Callable[..., Any]) -> Callable[..., Any]:
@@ -16,6 +17,38 @@ class Body:
     text: str
 
 
+@dataclass(frozen=True)
+class SendResult:
+    id: str  # The ID for the message
+    status: str  # The status of the message (e.g. sent, queued, failed).
+
+
 @post(path="/send-text")
-def send_text(body: Body) -> dict[str, Body]:
-    return {"body": body}
+def send_text(body: Body) -> SendResult:
+    return send("", "sms", body.to, body.from_, body.text)
+    
+
+def send(
+    bearer_token: str, 
+    protocol: str, 
+    to: str,
+    from_: str,
+    message: str
+) -> SendResult:  
+    result = requests.post(
+        "https://t-e-x-t.example.com/send", 
+        headers={"Authorization": f"Bearer {bearer_token}"}, 
+        data={
+            "protocol": protocol,
+            "to": to,
+            "from": from_,
+            "message": message,
+        }
+    )
+    result.raise_for_status()
+    response = result.json()
+    return SendResult(
+        id=response["id"], 
+        status=response["status"],
+    )
+
