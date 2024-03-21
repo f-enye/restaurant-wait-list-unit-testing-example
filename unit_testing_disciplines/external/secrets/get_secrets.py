fb585805ac21eeaf89dba3bf1fad44118dfabe63
f-enye <fpena34@gmail.com>
2024-03-21 06:49:04 -0500

    "Use" google secrets manager as a dependency

--- unit_testing_disciplines/external/secrets/get_secrets.py
+++ unit_testing_disciplines/external/secrets/get_secrets.py
@@ -1,6 +1,26 @@
-def get_secrets(key: str) -> str:
-    """This function only serves to show how a dependency exists to obtain a value that is in a secret store.
+from google.cloud import secretmanager
+import google_crc32c
 
-    This function does not actually retrieve a secret from a secret store.
+class SecretDataCorruptionError(Exception):
+    pass
+
+def get_secrets(key: str) -> str:
+    """This function only serves to show how a dependency exists to obtain a value that is in a secret store (e.g. Google Secret Manager).
+
+    This example project is not actually hosted or dependent on any GCP products.
     """
-    return ""
+
+    client = secretmanager.SecretManagerServiceClient()
+    name = f"projects/some-example-project-id/secrets/{key}/versions/latest"
+
+    response = client.access_secret_version(request={"name": name})
+
+    _raise_for_invalid_payload_checksum(response)
+
+    return response.payload.data.decode("UTF-8")
+
+def _raise_for_invalid_payload_checksum(response):
+    crc32c = google_crc32c.Checksum()
+    crc32c.update(response.payload.data)
+    if response.payload.data_crc32c != int(crc32c.hexdigest(), 16):
+        raise SecretDataCorruptionError("Data corruption detected")
